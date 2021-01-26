e83b4ae4efd11b9b83306ee10a0e488dbe9a8865
sunny <sunnysunyan7@163.com>
2021-01-26 17:45:20 +0800

    framwork built 90%

---
+++ stories/mainConcepts/helloWorld.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const HelloWorld = () => {
+  return <button>123</button>
+}
+
+export default HelloWorld;
---
+++ stories/mainConcepts/index.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import HelloWorld from './helloWorld.js';
+
+export default {
+  title: 'mainConcepts'
+};
+
+export const helloWorld = () => <HelloWorld/>
